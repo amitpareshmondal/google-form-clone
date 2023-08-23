60c0a32b6367a080ac63331e4fcc6c17aa73c439
unknown <mandalamit325@gmail.com>
2023-08-23 15:53:43 +0530

    changed Dropdown

--- src/Components/DropDown/DropDown.js
+++ src/Components/DropDown/DropDown.js
@@ -6,10 +6,10 @@ const DropDown=()=>{
             <br/>
             <select name="cars" id="cars" style={{height:"50px",width:"150px",marginLeft:"20px",marginTop:'14px',border:"0.3px solid #F1F0E8",fontSize:"16px",paddingLeft:"15px",paddingRight:"15px",color:"grey"}}>
             <option value="" disabled selected>Choose</option>
-  <option value="volvo">Volvo</option>
-  <option value="saab">Saab</option>
-  <option value="mercedes">Mercedes</option>
-  <option value="audi">Audi</option>
+  <option value="volvo">Option 1</option>
+  <option value="saab">option 1</option>
+  <option value="mercedes">option 1</option>
+  <option value="audi">option 1</option>
 </select>
         </div>
     )
